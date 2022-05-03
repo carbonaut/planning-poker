a8df3eda8d108074ba930a84e5d30a50c2c84fe4
Melissa Fernandes <melfer100@gmail.com>
2022-05-03 17:32:29 -0300

    feat: create radio options buttons

---
+++ components/RadioOptions/radiooptions.tsx
@@ -0,0 +1,31 @@
+import styles from "./radiooptions.module.scss";
+import { useState } from "react";
+
+const RadioOptions = () => {
+  const [selected, setSelected] = useState("");
+  const hours = [1, 2, 3, 5, 8, 13, 21];
+
+  function onChange(e) {
+    setSelected(e.target.value);
+  }
+
+  return (
+    <div className={styles.options}>
+      {hours.map((hour) => (
+        <label key={hour} htmlFor={`${hour}`} className={styles.option}>
+          <input
+            className={styles.input}
+            id={`${hour}`}
+            type="radio"
+            name="voteHour"
+            value={hour}
+            onChange={onChange}
+          />
+          <span className={styles.text}>{hour}</span>
+        </label>
+      ))}
+    </div>
+  );
+};
+
+export default RadioOptions;
