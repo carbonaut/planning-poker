f56f19f7c0d2755e45dc925df7e30acde3496752
vitorhfs <vitorhugo.fernandes12@gmail.com>
2022-05-03 18:44:03 -0300

    feat: sets loading room on room page

--- components/Estimation/estimation.tsx
+++ components/Estimation/estimation.tsx
@@ -1,31 +1,30 @@
-import styles from './estimation.module.scss';
-import RadioOptions from '../RadioOptions/radiooptions'
-import { useState } from 'react';
-import Countdown from '../Countdown/countdown';
+import styles from "./estimation.module.scss";
+import RadioOptions from "../RadioOptions/radiooptions";
+import { useState } from "react";
+import Countdown from "../Countdown/countdown";
+import LoadingRoom from "../LoadingRoom/loadingRoom";
 
 const Estimation = () => {
-  const [ loading, setLoading ] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   return (
     <div className={styles.container}>
-      { loading ? 
-      <>
-        <p className={styles.title}>Aguardando inicio...</p>
-        <div className={styles.loadingIcon}>
-          <i className="bi bi-hourglass-split"></i>
-        </div>
-      </> :
-      <>
-        <p className={styles.title}>Estimativa de tarefa</p>
-        <p className={styles.description}>Quantas horas você acha que esta tarefa levará?</p>
-        <RadioOptions></RadioOptions>
-        <div className={styles.countdown}>
-          <Countdown duration={10} secondsLeft={10}></Countdown>
-        </div>
-      </>
-      }
+      {loading ? (
+        <LoadingRoom isScrumMaster={true} roomNumber={"651232"}></LoadingRoom>
+      ) : (
+        <>
+          <p className={styles.title}>Estimativa de tarefa</p>
+          <p className={styles.description}>
+            Quantas horas você acha que esta tarefa levará?
+          </p>
+          <RadioOptions></RadioOptions>
+          <div className={styles.countdown}>
+            <Countdown duration={10} secondsLeft={10}></Countdown>
+          </div>
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default Estimation;
---
+++ components/LoadingRoom/loadingRoom.tsx
@@ -0,0 +1,31 @@
+import styles from "./loadingRoom.module.scss";
+
+export interface LoadingProps {
+  isScrumMaster: boolean;
+  roomNumber: string;
+}
+
+const LoadingRoom = (props: LoadingProps) => {
+  return (
+    <>
+      {props.isScrumMaster ? (
+        <>
+          <p className={styles.title}>O ID da sala é:</p>
+          <div className={styles.dashed}>
+            <p> {props.roomNumber} </p>
+          </div>
+          <button className={styles.button}>Iniciar votação</button>
+        </>
+      ) : (
+        <>
+          <p className={styles.title}>Aguardando inicio...</p>
+          <div className={styles.loadingIcon}>
+            <i className="bi bi-hourglass-split"></i>
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+export default LoadingRoom;
