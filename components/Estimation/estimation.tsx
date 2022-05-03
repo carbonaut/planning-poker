9eb1f4db29322a9783662a0d5971ca16d91222fa
vitorhfs <vitorhugo.fernandes12@gmail.com>
2022-05-03 18:00:07 -0300

    feat: add countdown to room page

--- components/Estimation/estimation.tsx
+++ components/Estimation/estimation.tsx
@@ -1,19 +1,27 @@
 import styles from './estimation.module.scss';
+import RadioOptions from '../RadioOptions/radiooptions'
 import { useState } from 'react';
+import Countdown from '../Countdown/countdown';
 
 const Estimation = () => {
-  const [ loading, setLoading ] = useState(true);
+  const [ loading, setLoading ] = useState(false);
 
   return (
     <div className={styles.container}>
       { loading ? 
-      <div>
+      <>
         <p className={styles.title}>Aguardando inicio...</p>
         <div className={styles.loadingIcon}>
           <i className="bi bi-hourglass-split"></i>
         </div>
-      </div> :
-      <></>}
+      </> :
+      <>
+        <p className={styles.title}>Estimativa de tarefa</p>
+        <p className={styles.description}>Quantas horas você acha que esta tarefa levará?</p>
+        <RadioOptions></RadioOptions>
+        <Countdown duration={10} secondsLeft={10}></Countdown>
+      </>
+      }
     </div>
   )
 }
