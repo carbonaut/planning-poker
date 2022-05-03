07a739417f873591c4ab00c9fd8938ae9f7affff
Arthur <artfloriani@gmail.com>
2022-05-03 19:28:43 -0300

    refactor: update socket.io import

--- pages/room/[id].tsx
+++ pages/room/[id].tsx
@@ -1,28 +1,32 @@
 import { NextPage } from "next";
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
-import io from 'Socket.IO-client'
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
 import Estimation from "../../components/Estimation/estimation";
-import styles from '../../styles/Room.module.scss';
+import styles from "../../styles/Room.module.scss";
 
-let socket: any
+let socket: any;
 const Room: NextPage = () => {
-  const router = useRouter()
-  const { pid } = router.query
+  const router = useRouter();
+  const { pid } = router.query;
 
-  const [noDevs, setNoDevs] = useState(0)
-  useEffect(() => () => {socketInitializer()}, [])
+  const [noDevs, setNoDevs] = useState(0);
+  useEffect(
+    () => () => {
+      socketInitializer();
+    },
+    []
+  );
 
   const socketInitializer = async () => {
-    await fetch('/api/socket')
-    socket = io()
+    await fetch("/api/socket");
+    socket = io();
 
-    socket.on('connect', () => {
-      socket.emit('join', pid)
-    })
-  }
+    socket.on("connect", () => {
+      socket.emit("join", pid);
+    });
+  };
 
-  
   return (
     <div className={styles.container}>
       <div className={styles.content}>
@@ -33,7 +37,7 @@ const Room: NextPage = () => {
         <p className={styles.footer}>Criar uma sala</p>
       </footer>
     </div>
-  )
-}
+  );
+};
 
 export default Room;
