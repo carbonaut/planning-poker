3400f243c8afd0bf586341b109c7941160ba5927
Arthur <artfloriani@gmail.com>
2022-05-03 19:27:40 -0300

    refactor: remove socket.io from project

--- pages/example-room.tsx
+++ pages/example-room.tsx
@@ -1,45 +1,48 @@
-import type { NextPage } from 'next'
-import { useEffect, useState } from 'react'
-import io from 'Socket.IO-client'
-let socket: any
+import type { NextPage } from "next";
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+let socket: any;
 
 const Home: NextPage = () => {
-  const [noDevs, setNoDevs] = useState(0)
-  useEffect(() => () => {socketInitializer()}, [])
+  const [noDevs, setNoDevs] = useState(0);
+  useEffect(
+    () => () => {
+      socketInitializer();
+    },
+    []
+  );
 
   const socketInitializer = async () => {
-    await fetch('/api/socket')
-    socket = io()
+    await fetch("/api/socket");
+    socket = io();
 
-    socket.on('connect', () => {
-      socket.emit('join', '123')
-    })
+    socket.on("connect", () => {
+      socket.emit("join", "123");
+    });
 
-    socket.on('countUpdate', (data: any) => {
-        setNoDevs(data);
-    })
+    socket.on("countUpdate", (data: any) => {
+      setNoDevs(data);
+    });
 
-    socket.on('voteUpdate', (data: any) => {
-        console.log(data)
-    })
+    socket.on("voteUpdate", (data: any) => {
+      console.log(data);
+    });
 
-    socket.on('finished', (data: any) => {
-        console.log('finished!', data)
-    })
-  }
+    socket.on("finished", (data: any) => {
+      console.log("finished!", data);
+    });
+  };
 
   const handleVoteClick = () => {
-      socket.emit('vote', '5')
-  }
-
- 
+    socket.emit("vote", "5");
+  };
 
   return (
     <div>
       <h1>Room! Number of devs: {noDevs}</h1>
       <button onClick={handleVoteClick}>Vote!</button>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
