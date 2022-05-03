c0a614f1332e8f744f90205dc6ba94d4d46618a0
Arthur <artfloriani@gmail.com>
2022-05-03 19:18:48 -0300

    feat: adds basic results component

---
+++ components/ResultItem/result-item.tsx
@@ -0,0 +1,26 @@
+import styles from "./result-item.module.scss";
+
+interface ResultItemProps {
+  labelVotes: string;
+  labelItem: string;
+  selected?: boolean;
+  progress: number;
+  color: string;
+}
+
+const ResultItem = (props: ResultItemProps) => {
+  return (
+    <div className={styles.item}>
+      <div className={styles.text}>{props.labelItem}</div>
+      <div className={styles.bar}>
+        <div
+          className={styles.progress}
+          style={{ width: `${props.progress}%`, backgroundColor: props.color }}
+        ></div>
+        <div className={styles.label}>{props.labelVotes}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ResultItem;
---
+++ components/Results/results.tsx
@@ -0,0 +1,42 @@
+import ResultItem from "../ResultItem/result-item";
+
+import styles from "./results.module.scss";
+
+/**
+ * votes.label -> Estimation number (1,2,3,5...)
+ * votes.count -> Number of votes for that estimation
+ * votes.color -> Background color for that estimation
+ */
+interface ResultsProps {
+  votes: { label: string; count: number; color: string }[];
+}
+
+const Results = (props: ResultsProps) => {
+  const totalVotes = props.votes.reduce(
+    (currentValue, el) => currentValue + el.count,
+    0
+  );
+  console.log(totalVotes);
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Resultados</h2>
+      <div className={styles.content}>
+        {props.votes.map((el, index) => {
+          const progress = (el.count / totalVotes) * 100;
+          const labelVoto = el.count === 1 ? "voto" : "votos";
+          return (
+            <ResultItem
+              key={index}
+              labelVotes={`${el.count} ${labelVoto}`}
+              color={el.color}
+              labelItem={el.label}
+              progress={progress}
+            ></ResultItem>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Results;
