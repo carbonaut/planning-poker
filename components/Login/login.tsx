3edfadd4b0e53542eb1a7eb151f8099f4e71e845
Arthur <artfloriani@gmail.com>
2022-05-17 17:44:42 -0300

    feat(login): adds new design on btn

--- components/Login/login.tsx
+++ components/Login/login.tsx
@@ -1,16 +1,30 @@
-import { useState } from 'react';
-import styles from './login.module.scss';
+import { useState } from "react";
+import styles from "./login.module.scss";
 
 const Login = (props: any) => {
-  const [value, setValue] = useState('')
+  const [value, setValue] = useState("");
 
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>CarboLab</h2>
-      <input className={styles.input} type="text" placeholder="ID da sala" onChange={ev => setValue(ev.target.value) }/>
-      <button className={styles.button} onClick={() => {props.onVisitRoom(value)}}>Entrar</button>
+      <input
+        className={styles.input}
+        type="text"
+        placeholder="ID da sala"
+        onChange={(ev) => setValue(ev.target.value)}
+      />
+      <button
+        className={`${styles.button} ${
+          value.length == 0 ? styles.disabled : ""
+        }`}
+        onClick={() => {
+          props.onVisitRoom(value);
+        }}
+      >
+        Entrar
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
