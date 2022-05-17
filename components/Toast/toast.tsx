6931ac1bba70a454427b3c09b783de377352cafc
Arthur <artfloriani@gmail.com>
2022-05-17 17:02:12 -0300

    feat(toast): add toast message to home page

---
+++ components/Toast/toast.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import styles from "./toast.module.scss";
+
+interface ToastProps {
+  type?: string; // error, success
+  visible: boolean;
+  message?: string;
+  onHide?: any;
+}
+
+const Toast = (props: ToastProps) => {
+  return (
+    <div
+      className={`${styles.container} bg-${props.type} ${
+        props.visible ? styles.active : ""
+      }`}
+    >
+      <div className={styles.text}>{props.message}</div>
+      <div className={styles.button}>
+        <button className="button button-icon" onClick={props.onHide}>
+          <i className="bi bi-x"></i>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Toast;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,21 @@ import Router from "next/router";
 import Head from "next/head";
 import Login from "../components/Login/login";
 import styles from "../styles/Home.module.scss";
+
+import Toast from "../components/Toast/toast";
+import { useState } from "react";
 import io from "socket.io-client";
 import { useEffect } from "react";
+
 let socket: any;
 
 const Home: NextPage = () => {
+  const [errorMessage, setErrorMessage] = useState({
+    message: "",
+    type: "",
+    visible: false,
+  });
+  const [toastTimeout, setToastTimeout] = useState();
   useEffect(
     () => () => {
       socketInitializer();
@@ -45,10 +55,48 @@ const Home: NextPage = () => {
   const visitRoom = (roomId: string): void => {
     Router.push(`/room/${roomId}`);
     sessionStorage.setItem("isHost", "false");
+    Router.push(`room/${roomId}`);
+  };
+
+  /**
+   * Shows a toast message when an error occurs
+   * @param message text to be shown as a toast message
+   */
+  const showError = (message: string) => {
+    // avoids hiding a toast before the 3s ends
+    if (toastTimeout) {
+      clearTimeout(toastTimeout);
+      setToastTimeout(undefined);
+    }
+
+    // show the toast message
+    setErrorMessage({ message, type: "error", visible: true });
+
+    // hides toast message after 3 seconds
+    const eTimeout: any = setTimeout(() => {
+      console.log(toastTimeout);
+      hideToast();
+    }, 3000);
+
+    setToastTimeout(eTimeout);
+  };
+
+  const hideToast = () => {
+    setErrorMessage({ ...errorMessage, visible: false });
+    // clears the running timeout
+    clearTimeout(toastTimeout);
+    setToastTimeout(undefined);
   };
 
   return (
     <div className={styles.container}>
+      <Toast
+        message={errorMessage.message}
+        visible={errorMessage.visible}
+        type={errorMessage.type}
+        onHide={hideToast}
+      ></Toast>
+
       <Head>
         <title>Planning Poker</title>
         <meta name="description" content="Estimation app from Carbonaut" />
