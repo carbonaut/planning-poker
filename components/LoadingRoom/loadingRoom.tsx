c73ca6858d55b4f7a983dfcc99d05dda2eacd16f
Melissa Fernandes <melfer100@gmail.com>
2022-06-28 10:55:34 -0300

    feat: implement copy id experience

--- components/LoadingRoom/loadingRoom.tsx
+++ components/LoadingRoom/loadingRoom.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styles from "./loadingRoom.module.scss";
 
 export interface LoadingProps {
@@ -7,24 +8,47 @@ export interface LoadingProps {
 }
 
 const LoadingRoom = (props: LoadingProps) => {
+  const [copied, setCopy] = useState(false);
+
   async function copyID() {
+    let link = window.location.href;
+
+    if (link.includes("?host=true")) {
+      link = link.replace("?host=true", "");
+    }
+
     // catch for chrome
-    await navigator.clipboard.writeText(props.roomNumber)
-    .then(() => {
-      // success toast
-    })
-    .catch(() => {
-      // error toast
-    })
+    await navigator.clipboard
+      .writeText(link)
+      .then(() => {
+        setCopy(true);
+        setTimeout(() => {
+          setCopy(false);
+        }, 1000);
+      })
+      .catch(() => {
+        // error toast
+      });
   }
-  
+
   return (
     <>
       {props.isScrumMaster ? (
         <>
           <p className={styles.title}>O ID da sala é:</p>
           <div className={styles.dashed}>
-            <p onClick={copyID}> {props.roomNumber} </p>
+            <p onClick={copyID}>
+              {" "}
+              {props.roomNumber}{" "}
+              {!copied && (
+                <i className={`bi bi-front ${styles.dashedIcon}`}></i>
+              )}
+              {copied && (
+                <i
+                  className={`bi bi-check-circle-fill ${styles.dashedIcon}`}
+                ></i>
+              )}
+            </p>
           </div>
           <button className={styles.button} onClick={props.onStart}>
             Iniciar votação
