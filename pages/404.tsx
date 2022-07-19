5f3cc09e78d912c93d974744ef12036f23f2b13e
vitorhfs <vitorhugo.fernandes12@gmail.com>
2022-07-19 11:52:35 -0300

    feat: remove 404 attempt

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,22 +2,17 @@ import Router, { useRouter } from "next/router";
 import { useEffect } from "react";
 
 export default function NotFound() {
-  const router = useRouter();
-
-  useEffect(() => {
-    const { pathname, asPath, isReady } = router;
-    const roomPathMatch = pathname.match(/[/room/[id]]/g);
-
-    if (roomPathMatch && isReady) {
-      Router.push(asPath);
-
-      return;
-    }
-
-    if (isReady) {
-      Router.push("/");
-    }
-
-    return;
-  }, [router]);
+  // const router = useRouter();
+  // useEffect(() => {
+  //   const { pathname, asPath, isReady } = router;
+  //   const roomPathMatch = pathname.match(/[/room/[id]]/g);
+  //   if (roomPathMatch && isReady) {
+  //     Router.push(asPath);
+  //     return;
+  //   }
+  //   if (isReady) {
+  //     Router.push("/");
+  //   }
+  //   return;
+  // }, [router]);
 }
