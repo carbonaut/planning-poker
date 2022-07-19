1bcd95d44130b4a51eefd88a865a0e7594d9a903
vitorhfs <vitorhugo.fernandes12@gmail.com>
2022-07-19 11:25:37 -0300

    fix: app interceptor attempt

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,18 +2,11 @@ import Router from "next/router";
 import { useEffect } from "react";
 
 export default function NotFound() {
-  useEffect(() => {
-    const { pathname } = Router;
-    const roomPathMatch = pathname.match(/[/room/(.+)]/g);
-
-    if (roomPathMatch) {
-      Router.push(pathname);
-      return;
-    }
-
-    if (pathname !== "/" && !pathname.includes("/room/")) {
-      Router.push("/");
-    }
-  });
-  return;
+  // useEffect(() => {
+  //   const { pathname } = Router;
+  //   if (pathname !== "/" && !pathname.includes("/room/")) {
+  //     Router.push("/");
+  //   }
+  //   return;
+  // })
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,29 @@ import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { ToastContext } from "../utils/ToastContext";
+import { useEffect, useState } from "react";
+import Router, { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const { pathname, asPath, isReady } = router;
+    const roomPathMatch = pathname.match(/[/room/[id]]/g);
+
+    if (roomPathMatch && isReady) {
+      Router.push(asPath);
+
+      return;
+    }
+
+    if (isReady) {
+      Router.push("/");
+    }
+
+    return;
+  }, [router]);
+
   return (
     <ToastContext>
       <Head>
