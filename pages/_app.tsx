d513aab3e872722031c9ee3067343ff94c32be07
vitorhfs <vitorhugo.fernandes12@gmail.com>
2022-07-19 12:08:22 -0300

    feat: remove trailing slash

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,6 @@ import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { ToastContext } from "../utils/ToastContext";
-import { useEffect, useState } from "react";
-import Router, { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
